49a4fa44edf124f98dee97abd1ccf8791d018464
Shan <Shan@DESKTOP-S659FNH>
2024-06-13 14:50:33 +0530

    Lakshmi bank

---
+++ BankDetails/src/org/bank/AxisBank.java
@@ -0,0 +1,15 @@
+package org.bank;
+
+public class AxisBank {
+	public void deposit() {
+		// TODO Auto-generated method stub
+System.out.println("Axis bank is for deposit the money ");
+	}
+	public static void main(String[] args) {
+		AxisBank a = new AxisBank();
+		a.deposit();
+	}
+	
+	
+
+}
---
+++ BankDetails/src/org/bank/BankInfo.java
@@ -0,0 +1,42 @@
+package org.bank;
+
+public class BankInfo extends AxisBank {
+	public void saving() {
+	// TODO Auto-generated method stub
+System.out.println("Saving acc is in Axis Bank");
+}
+	public void fixed() {
+	// TODO Auto-generated method stub
+		System.out.println("Fixed is in Axis Bank");
+
+}
+public static void main(String[] args) {
+	BankInfo bi = new BankInfo();
+	bi.saving();
+	bi.fixed();
+	bi.deposit();
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
